57b4b7d31f6db7fee3f19f2626777035252722fd
Mark Condello <mark@dcode.com.au>
2022-03-27 13:17:54 +1100

    added .config file

--- src/generateJoke.js
+++ src/generateJoke.js
@@ -1,6 +1,6 @@
 
 let generateJoke = () => {
-  return "I don't trust stairs. They are always up to smethings..";
+  return "I don't trust stairs. They are always up to something..";
 }
 
 export default generateJoke;
